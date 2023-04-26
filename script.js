4efdafa27cc5c70c47a7d1f767c3297e57323802
Tano Paul <tanopaul38@gmail.com>
2023-04-26 14:29:46 -0400

    added ids to elements for js, created variables to link to ids in index.html

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-const h2 = document.createElement('h2');
-h2.textContent = 'This content added by Javascript';
+// const h2 = document.createElement('h2');
+// h2.textContent = 'This content added by Javascript';
 
-document.querySelector('body').appendChild(h2);
+// document.querySelector('body').appendChild(h2);
+
+const carousel = document.getElementById('carousel-image');
+const leftArrow = document.getElementById('left');
+const rightArrow = document.getElementById('right');
+const guamButton = document.getElementById('guam');
+const thailandButton = document.getElementById('thailand');
+const peruButton = document.getElementById('peru');
+const romaniaButton = document.getElementById('romania');
+const travelImage = document.getElementById('travel-image');
+
+
+guamButton.addEventListener('click', function() {
+    alert('guam!')
+});
+
+thailandButton.addEventListener('click', function() {
+    alert('thailand!')
+});
+
+peruButton.addEventListener('click', function() {
+    alert('peru!')
+});
+
+romaniaButton.addEventListener('click', function() {
+    alert('romania!')
+});
+
+rightArrow.addEventListener('click', function() {
+    alert('clicked!')
+});
+
+leftArrow.addEventListener('click', function() {
+    alert('clicked!')
+});
