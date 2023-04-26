ca0b252ba93f8ea2180e6a135a0d94cad229b539
Tano Paul <tanopaul38@gmail.com>
2023-04-26 14:57:35 -0400

    added photo variables to script.js, change photo src with 'click' event listener

--- script.js
+++ script.js
@@ -11,22 +11,29 @@ const thailandButton = document.getElementById('thailand');
 const peruButton = document.getElementById('peru');
 const romaniaButton = document.getElementById('romania');
 const travelImage = document.getElementById('travel-image');
+const guamPhoto = 'file:///Users/tanopaul/Development/code/my-personal-website/images/guam.png';
+const thailandPhoto = 'file:///Users/tanopaul/Development/code/my-personal-website/images/elephant2.png';
+const peruPhoto = 'file:///Users/tanopaul/Development/code/my-personal-website/images/peru2.png';
+const romaniaPhoto = 'file:///Users/tanopaul/Development/code/my-personal-website/images/romania.png';
+
+
+const travelArray = [guamPhoto, thailandPhoto, peruPhoto, romaniaPhoto];
 
 
 guamButton.addEventListener('click', function() {
-    alert('guam!')
+    travelImage.src = travelArray[0];
 });
 
 thailandButton.addEventListener('click', function() {
-    alert('thailand!')
+    travelImage.src = travelArray[1];
 });
 
 peruButton.addEventListener('click', function() {
-    alert('peru!')
+    travelImage.src = travelArray[2];
 });
 
 romaniaButton.addEventListener('click', function() {
-    alert('romania!')
+    travelImage.src = travelArray[3];
 });
 
 rightArrow.addEventListener('click', function() {
